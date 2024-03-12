4e095a4125ef1e56c39e75c8d6f70f95ee1775fe
Mirko Bozzetto <mirkouch@gmail.com>
2024-03-12 19:57:50 +0100

    Ajouter la fonction maxChar pour trouver le caractère le plus fréquent dans une chaîne de caractères

--- exercises/3-maxchars.js
+++ exercises/3-maxchars.js
@@ -5,4 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char] = charMap[char] + 1;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+  console.log(charMap);
+
+  for (const [key, value] of Object.entries(charMap)) {
+    if (value > max) {
+      max = value;
+      maxChar = key;
+    }
+  }
+  return maxChar;
+}
+
+console.log(maxChar("hello"));
