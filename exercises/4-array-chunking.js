8fed52b0725c6ef77cff4ada2156b59368d72fa2
Mirko Bozzetto <mirkouch@gmail.com>
2024-03-12 23:28:19 +0100

    Ajouter la fonction de découpage d'un tableau en morceaux

--- exercises/4-array-chunking.js
+++ exercises/4-array-chunking.js
@@ -8,4 +8,22 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const result = [];
+  let index = 0;
+
+  while (index < array.length) {
+    result.push(array.slice(index, index + size));
+    index += size;
+  }
+  return result;
+}
+// ^ un autre ...
+// function chunk(array, size) {
+//   return Array.from({ length: Math.ceil(array.length / size) }, (_, index) =>
+//     array.slice(index * size, index * size + size)
+//   );
+// }
+
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3));
+console.log(chunk([1, 2, 3, 4], 2));
