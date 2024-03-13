0e3a86b4663b61ba1658247e4d60104538bc8d33
Mirko Bozzetto <mirkouch@gmail.com>
2024-03-13 21:52:14 +0100

    Ajouter une fonction pour compter les voyelles dans une chaîne de caractères

--- exercises/7-vowels.js
+++ exercises/7-vowels.js
@@ -6,8 +6,21 @@
 //   vowels('Hi There!') --> 3
 //   vowels('How are you?') --> 5
 //   vowels('Coding Money') --> 4
-//   vowels('why?') --> 0 
+//   vowels('why?') --> 0
 
-function vowels(str) {}
+// * the shorter
+// function vowels(str) {
+//   const count = str.match(/[aeiouy]/gi).length;
+//   return count;
+// }
 
-console.log(vowels('Coding Money'));
+function vowels(str) {
+  const vowelCheck = ["a", "e", "i", "o", "u", "y"];
+  let count = 0;
+  for (let char of str.toLowerCase()) {
+    if (vowelCheck.includes(char)) count++;
+  }
+  return count;
+}
+
+console.log(vowels("Coding Money"));
