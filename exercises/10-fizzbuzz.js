6278dfca8a581b5f13fcbe478a73ad06c0f2efa0
Mirko Bozzetto <mirkouch@gmail.com>
2024-03-13 22:48:29 +0100

    Mise à jour de la fonction fizzBuzz pour générer une séquence de nombres et de mots "fizz" et "Buzz" correctement.

--- exercises/10-fizzbuzz.js
+++ exercises/10-fizzbuzz.js
@@ -12,6 +12,34 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+// function fizzBuzz(n) {
+//   let fizz = "fizz";
+//   let buzz = "Buzz";
+//   let content = "";
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 15 === 0) {
+//       content += fizz + buzz;
+//     } else if (i % 3 === 0) {
+//       content += fizz;
+//     } else if (i % 5 === 0) {
+//       content += buzz;
+//     } else {
+//       content += i;
+//     }
+//     content += "\n";
+//   }
+//   return content;
 
-fizzBuzz(5)
+function fizzBuzz(n) {
+  let result = "";
+  for (let i = 0; i <= n; i++) {
+    let output = "";
+    if (i % 3 === 0) output += "fizz";
+    if (i % 5 === 0) output += "Buzz";
+    if (output === "") output = i;
+    result += output + "\n";
+  }
+  return result;
+}
+
+console.log(fizzBuzz(150));
