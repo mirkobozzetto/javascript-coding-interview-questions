6efcf8c9688cde811f57c54a3a47ea1a0b27fa93
Mirko Bozzetto <mirkouch@gmail.com>
2024-03-14 12:31:18 +0100

    Ajouter la logique pour générer une matrice en spirale

--- exercises/11-the-matrix.js
+++ exercises/11-the-matrix.js
@@ -15,6 +15,30 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const result = [];
+  let counter = 1,
+    startRow = 0,
+    endRow = n - 1,
+    startCol = 0,
+    endCol = n - 1;
+
+  //
+  for (let i = 0; i < n; i++) {
+    result.push([]);
+  }
+  // top
+  for (i = startCol; i <= endCol; i++) {
+    result[startRow][i] = counter;
+  }
+  // right
+
+  // bottom
+
+  // left
+  return result;
+}
 
 console.log(matrix(4));
+// 1:47:00 / 1:52:52
+// https://www.youtube.com/watch?v=ufBbWIyKY2E&t=602s
